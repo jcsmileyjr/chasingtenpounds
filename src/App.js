9a1d3a9d97df5801cd9c559b8b12b59c3bba2fce
JC Smiley <jcsmileyjr@gmail.com>
2020-03-07 11:29:15 -0600

    Create Dark Button component for header
    - There are two main buttons on the landing page, a light (body) and dark button (header). 
    - Bug: bootstrap.css file posted in the wrong file
    - Temporary display of button to App.js for testing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import DarkButton from './components/DarkButton';
 import './App.css';
 
 function App() {
+
+  const test =()=> {
+    alert("Hello World");
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <DarkButton title="Sign Up" action={test} />
     </div>
   );
 }
