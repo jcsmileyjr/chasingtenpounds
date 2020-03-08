7dceac7dda2ce925f9fb73b024d1e293cc81a358
JC Smiley <jcsmileyjr@gmail.com>
2020-03-08 12:27:58 -0500

    Feat. Section 1 of Landing Page: initial element placement and styling

---
+++ src/pages/landingPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import LandingPageHeader from '../components/LandingPageHeader';
+import LandingPageBody from '../components/LandingPageBody';
+
+const LandingPage = () => {
+    return(
+        <div>
+            <LandingPageHeader />
+            <LandingPageBody />
+        </div>
+    );
+}
+
+export default LandingPage;
