569adb775a71cfbbfd3015c01e64eb3bdcd5e692
JC Smiley <jcsmileyjr@gmail.com>
2020-03-07 23:54:36 -0500

    Feat. Header so elements is responsive
    - Align buttons to the far right of Header
    - All elements mobile, tablet, and desktop responsive

--- src/components/LandingPageHeader.js
+++ src/components/LandingPageHeader.js
@@ -2,8 +2,8 @@ import React from 'react';
 import './LandingPageHeader.css';
 
 import BlueButton from './BlueButton';
-import {Container, Row, Col} from 'react-bootstrap';
-
+import {Container, Row, Col, Image, h1} from 'react-bootstrap';
+import monster from '../assets/images/monster4.png';
 
 const LandingPageHeader = () => {
     const test = () =>{
@@ -12,11 +12,16 @@ const LandingPageHeader = () => {
 
     return (
         <Container fluid={true} className="background">
-            <Row>
-                <Col>Hello</Col>
-                <Col>Chasing the <span className="redText">10 lb</span> Monster</Col>
-                <Col>
+            <Row className="content">
+                <Col xs={6} sm={2} md={2} lg={true} xl={true}>
+                    <Image src={monster} fluid className="imageStyle"/>
+                </Col>
+                <Col xs={6} sm={5} md={5} lg={6} xl={true}>
+                    <h1 className="mobileTitleTextSize">Chasing the <span className="redText">10 lb</span> Monster</h1></Col>
+                <Col xs={6} sm={true} md={true} lg={true} xl={true} className="rightAlignButtons paddingBetweenLogoButtons">
                     <BlueButton buttonType="dark" action={test} title="Sign Up" />
+                </Col>
+                <Col xs={6} sm={true} md={true} lg={true} xl={true} className="rightAlignButtons">
                     <BlueButton buttonType="dark" action={test} title="Log In" />
                 </Col>
             </Row>
