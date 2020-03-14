86a92c01750c3540d7c5777a8d7a0755197af905
JC Smiley <jcsmileyjr@gmail.com>
2020-03-14 09:19:04 -0500

    Refactor styling of WeighInBody to fix bugs
    - Switch from using "Absolute" CSS to Flex box
    - Footer would not stick to buttum of page
    - elements in body would not center together

--- src/components/WeighInPageBody.js
+++ src/components/WeighInPageBody.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './WeighInPageBody.css'
 import {Container, Row, Col, Form} from 'react-bootstrap';
 import BlueButton from '../components/BlueButton';
+import Footer from '../components/LandingPageFooter';
 
 const WeighInPageBody = () => {
     /*FOR TESTING ONLY */
@@ -9,23 +10,24 @@ const WeighInPageBody = () => {
         console.log("WIP: Testing that the button works!");
     }
     return (
-        <Container fluid={true} className="weighInCenterForm">
+        <Container fluid={true} className="weighInPageLayout">
             <Row className="weighInBody">
                 <Col xs={12} >
                     <h2 className="weighInTitle">Type in Today's Weight</h2>
                 </Col>
-                <Col xs={12} className="centerElements weighInWhiteSpaceAbove">
+                <Col xs={12} sm={{ span: 6, offset: 3 }} md={{ span: 4, offset: 4 }} className="centerElements weighInWhiteSpaceAbove">
                     <Form.Group controlId="UserPassword">
-                        <Form.Control type="number" placeholder="Type in your weight" required />
+                        <Form.Control type="number" placeholder="Type in your weight" required  />
                     </Form.Group>
                 </Col>
-                <Col xs={12} className="centerElements weighInWhiteSpaceAbove">
+                <Col xs={12} sm={{ span: 6, offset: 3 }} md={{ span: 4, offset: 4 }} className="centerElements weighInWhiteSpaceAbove">
                     <BlueButton buttonType="light" action={test} title="Submit New Weight" flat={true} wide={true}/>
                 </Col>
             </Row>
-            <Row className="weighInFooter">
-                <Col>Hello</Col>
-            </Row>  
+            <Row>
+                <Footer />
+            </Row>
+             
         </Container>
     );
 }
