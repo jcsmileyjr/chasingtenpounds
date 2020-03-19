746b1a711382d272e8901543ddd3e58f3b3d41e9
JC Smiley <jcsmileyjr@gmail.com>
2020-03-19 13:42:41 -0500

    User-Feedback: to many char on a single line on large screens

--- src/components/LandingPageBody.js
+++ src/components/LandingPageBody.js
@@ -28,7 +28,7 @@ const LandingPageBody = () => {
                 <Col xs={{span:12, order:1}} sm={{order:2}} md={{order:2}} lg={{order:2}} xl={{span: 8,order:2}} className="centerContent">
                     <p className="CTATitle">Do you want to Chase the <span style={{color:"#FF0000"}}>10 Pound </span>Monster?</p>
                     <p>Challenge Family, Friends, & Co-Workers to see who can <span style={{color:"#FF0000", fontWeight:500}}>lose 10 lbs the Fastest.</span></p>
-                    <p>10 pounds is an attainable, measureable, and visible fitness goal. 
+                    <p className="limitChars">10 pounds is an attainable, measureable, and visible fitness goal. 
                     The best part is it can be done in a short time period. So choose, do 
                     you want or need to chase the 10 Pound Monster?
                     </p>
