fd753de05430681b84a5a6e28e0af9345ea7db5f
Smiley <jcsmileyjr@gmail.com>
2020-08-14 17:36:07 -0500

    Refactor to route user to different screens based on if email is known
    Used dummy information

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useEffect} from 'react';
 import {Route, useHistory} from "react-router-dom";
 import LandingPage from './pages/LandingPage1';
 import LogInPage from './pages/LogInPage';
@@ -9,25 +9,32 @@ import RankingPage from './pages/RankingPage';
 import './App.css';
 import { useAuth0 } from '@auth0/auth0-react';
 
+const savedUsers = [
+  'jcsmileyjr@gmail.com',
+  'jsmiley@bellsouth.net'
+]
+
 const App = () => {
   const history = useHistory()
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const {isAuthenticated, loginWithRedirect, user} = useAuth0();
 
   // Check if the user is authenenticated every time the page is loaded, if so logs user information and true/false
   useEffect(() => {
     if (isAuthenticated) {
-      setIsLoggedIn(true);
-      history.push("/weighIn");
+      isSavedUser(user.email);
     } 
-    console.log(isLoggedIn);
-    console.log(user);
-  }, [isAuthenticated, isLoggedIn, user, history]);
+  }, );
 
   // Use Auth0 to log in the user
-  const logUserIn = async () => {
+  const logUserIn = () => {
     loginWithRedirect();
+  }
+
+  const isSavedUser = (value) => {
+    console.log(`User email is ${value}`)
+    const ifValid = savedUsers.includes(value);
+    return ifValid ? history.push('/weighIn'): history.push('/signUp');
   }
 
   return (
@@ -39,13 +46,9 @@ const App = () => {
       />
       <Route path="/logIn" component={LogInPage} />
       <Route path="/signUp" component={SignUpPage} />
-      {isLoggedIn && (
-        <div>
-          <Route path="/weighIn" component={WeighInPage} />
-          <Route path="/team" component={TeamPage} />
-          <Route path="/ranking" component={RankingPage} />
-        </div>
-      )}
+      <Route path="/weighIn" component={WeighInPage} />
+      <Route path="/team" component={TeamPage} />
+      <Route path="/ranking" component={RankingPage} />
     </div>
   );
 };
