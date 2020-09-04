3d779999a9b244ea1d3a55dae46746d5afafbf80
Smiley <jcsmileyjr@gmail.com>
2020-09-04 08:49:19 -0500

    Bug: quote was changing with every keypad stroke

--- src/components/QuoteMonster.js
+++ src/components/QuoteMonster.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {Col, Image} from 'react-bootstrap';
 import monsterQuote from '../assets/images/monsterQuote.png';
 import "./QuoteMonster.css";
@@ -19,13 +19,17 @@ var inspiration = [
 ];
 
 const QuoteMonster = () => {
+    const [quote, setQuote] = useState('');
+
+    useEffect(() => { setQuote(randomQuote())  }, []);
+
     const randomQuote = () => {
         return inspiration[Math.floor((Math.random() * inspiration.length-1 ) + 1)];
     }
     return(
         <Col xs={12} sm={{ span: 8, offset: 2 }} md={{ span: 6, offset: 3 }} className="whiteSpaceBelow">
             <p className="quoteTitleStyle">Inspirational Quote</p>
-            <p className="centerElement quote">{randomQuote()}</p>
+            <p className="centerElement quote">{quote}</p>
             <article style={{textAlign: "right", marginTop: "-1rem"}}>
                 <Image src={monsterQuote} fluid className="quoteMonster" />
             </article>                    
