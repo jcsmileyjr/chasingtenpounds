970517989391f48a98b81b3c53fa442cba60ddab
Smiley <jcsmileyjr@gmail.com>
2020-09-04 14:31:39 -0500

    Add current number of weeks and documentation to other functions

--- functions/Login.js
+++ functions/Login.js
@@ -17,7 +17,7 @@ const getTeamStartDates = async () => {
 
 exports.handler = async function(event, context, callback) {
     const userEmail = JSON.parse(event.body);
-    const startDates = await getTeamStartDates(); // Make API call to database and get all users.
+    const startDates = await getTeamStartDates(); // Make API call to database and get teams names/start dates.
     const Users = await getRecords(); // Make API call to database and get all users.
     const ifValid = checkIfSignedUp(userEmail, Users); // Check if the authenicated user is a valid player
     const data = ifValid ? organizeTeamData(userEmail, Users, startDates) : [];// if user is valid, return organize teams else a blank array
@@ -50,17 +50,15 @@ exports.handler = async function(event, context, callback) {
     return signedUp;
   }
 
+  // Function to get the number of weeks a competition has been playing for
   const getCurrentWeek = (startDates, userTeamName) => {
-    const currentTeam = startDates.find(team => team.fields.teamName === userTeamName);
-//console.log(currentTeam);
-//console.log(`The ${userTeamName} start date is ${currentTeam.fields.startDate}`);
+    const currentTeam = startDates.find(team => team.fields.teamName === userTeamName);// find ateam from the online database and compare to current team
     if(currentTeam === undefined){
       return 1;
     }else{
-      const dateArray = currentTeam.fields.startDate.split('-');
+      const dateArray = currentTeam.fields.startDate.split('-'); // Fix a date bug. Example is 8-14-2020, which dose not translate into a date()
       const newDateArray = `${dateArray[1]}/${dateArray[2]}/${dateArray[0]}`
       const todayDate = new Date();
-  //console.log(newDateArray)
       const minutesInADay = 24*60*60*1000;
       const officialTeamStartDate = new Date(newDateArray);
       const numberOfDays = Math.round(Math.abs((todayDate.getTime() - officialTeamStartDate.getTime())/(minutesInADay)));
@@ -74,30 +72,29 @@ exports.handler = async function(event, context, callback) {
     }
   }
 
+  // Based on the player's last update, the number of days since then is return (all in milliseconds)
+  const determineLastUpdate = lastUpdate => {
+    const minutesInADay = 86400000; // Number of milliseconds in a day
+    return Math.round(Math.abs((Date.parse(new Date()) - Date.parse(lastUpdate))/minutesInADay))
+  }
+
   // Based on the current user, organize the data by their teams
   const organizeTeamData = (userEmail, Users, startDates) => {
     let displayTeams = []; // Array of teams
     let playerTeams = []; // Names of the team the player is on
 
-    const currentUser = Users.find(player => player.fields.email === userEmail); // Find current player from database of players
-    
+    const currentUser = Users.find(player => player.fields.email === userEmail); // Find current player from database of players    
     playerTeams = currentUser.fields.teams.split(','); // Get current player teams and convert into an array
-
-    
 
     // Create array of array of players by team name
     playerTeams.forEach(team => {
       let teamDetails = {};
       teamDetails.teamName = team;
-console.log(team);
-      teamDetails.currentWeek = getCurrentWeek(startDates, team);/**TODO: Functionality & data to dynamically get current week */
-console.log(teamDetails.currentWeek);
+      teamDetails.currentWeek = getCurrentWeek(startDates, team);/**dynamically get current week */
       let teamOfPlayers = [];
       Users.forEach(player => {
         const checkIfOnSameTeam = player.fields.teams.includes(team);
-        const minutesInADay = 86400000; // Number of milliseconds in a day
-        // Today Date in milliseconds - minus the player last update date divide by minutes in a day
-        const daysSinceLastUpdate = Math.round(Math.abs((Date.parse(new Date()) - Date.parse(player.fields.lastUpdate))/minutesInADay))
+        const daysSinceLastUpdate = determineLastUpdate(player.fields.lastUpdate) 
         if(checkIfOnSameTeam){
           /*Strip all players of un-needed data*/
           const sanitizedPlayer = {};
@@ -108,8 +105,8 @@ console.log(teamDetails.currentWeek);
           teamOfPlayers.push(sanitizedPlayer);
         }
       });
-      teamDetails.players = teamOfPlayers; // Add teams of players to array of teams
 
+      teamDetails.players = teamOfPlayers; // Add teams of players to array of teams
       displayTeams.push(teamDetails);// Add teams of players to array of teams
     })
     return displayTeams;
--- functions/UpdateWeight.js
+++ functions/UpdateWeight.js
@@ -12,28 +12,35 @@ const getRecords = async () => {
 const updateRecord = async (fields) => {
   await table.update([fields]);
 }
+
+const teamTable = base('teams');
+
+const getTeamStartDates = async () => {
+  const teamStartDates = await teamTable.select().firstPage();
+  return teamStartDates;
+}
   
-  exports.handler = async function(event, context, callback) {
-    const userData = JSON.parse(event.body);
+exports.handler = async function(event, context, callback) {
+  const userData = JSON.parse(event.body);
 
-    const Users = await getRecords(); // Make API call to database and get all users.
+  const Users = await getRecords(); // Make API call to database and get all users.
+  const startDates = await getTeamStartDates(); // Make API call to database and get teams names/start dates.
+  
+  updatePlayerInDatabase(userData, Users); // Make API call to update player information in the database
 
-    // Make API call to update player information in the database
-    updatePlayerInDatabase(userData, Users);
-
-    const updatedData = updateUserWeight(userData, Users);// Update user weight loss and organize all users into teams 
-    
-    callback(null, {
-      statusCode: 200,
-      headers: {
-        /* Required for CORS support to work */
-        'Access-Control-Allow-Origin': '*',
-        /* Required for cookies, authorization headers with HTTPS */
-        'Access-Control-Allow-Credentials': true
-      },
-      body: JSON.stringify(updatedData),
-    });
-  };
+  const updatedData = updateUserWeight(userData, Users, startDates);// Update user weight loss and organize all users into teams 
+  
+  callback(null, {
+    statusCode: 200,
+    headers: {
+      /* Required for CORS support to work */
+      'Access-Control-Allow-Origin': '*',
+      /* Required for cookies, authorization headers with HTTPS */
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify(updatedData),
+  });
+};
 
 // Update the current player's weight in the database
 const updatePlayerInDatabase = async (userData, Users) => {
@@ -55,7 +62,7 @@ const updatePlayerInDatabase = async (userData, Users) => {
 }
   
 // Based on the user's email, find the user in the data, update weight, update winner status, and then organize into teams
-const updateUserWeight = (newUserData, Users) => {
+const updateUserWeight = (newUserData, Users, startDates) => {
     const updatedUsers = Users.map((player) => {
       if(player.fields.email === newUserData.userEmail){
         const newWeghtLoss = player.fields.startWeight - newUserData.weight;
@@ -65,43 +72,69 @@ const updateUserWeight = (newUserData, Users) => {
       return player      
     });
 
-    const updatedState = organizeTeamData(newUserData.userEmail, updatedUsers);// Using the modifed Users array, return organized teams 
+    const updatedState = organizeTeamData(newUserData.userEmail, updatedUsers, startDates);// Using the modifed Users array, return organized teams 
 
     return updatedState;
   }
   
     
+  // Function to get the number of weeks a competition has been playing for
+  const getCurrentWeek = (startDates, userTeamName) => {
+    const currentTeam = startDates.find(team => team.fields.teamName === userTeamName);// find ateam from the online database and compare to current team
+    if(currentTeam === undefined){
+      return 1;
+    }else{
+      const dateArray = currentTeam.fields.startDate.split('-'); // Fix a date bug. Example is 8-14-2020, which dose not translate into a date()
+      const newDateArray = `${dateArray[1]}/${dateArray[2]}/${dateArray[0]}`
+      const todayDate = new Date();
+      const minutesInADay = 24*60*60*1000;
+      const officialTeamStartDate = new Date(newDateArray);
+      const numberOfDays = Math.round(Math.abs((todayDate.getTime() - officialTeamStartDate.getTime())/(minutesInADay)));
+
+      if(numberOfDays <= 7){
+        return 1; /*If challenge just started, it will output 1 instead of 0*/
+      }
+      else{
+        return Math.round(numberOfDays/7);/*round to nearest Interger the number of Days divided by 7*/
+      }
+    }
+  }
+
+  // Based on the player's last update, the number of days since then is return (all in milliseconds)
+  const determineLastUpdate = lastUpdate => {
+    const minutesInADay = 86400000; // Number of milliseconds in a day
+    return Math.round(Math.abs((Date.parse(new Date()) - Date.parse(lastUpdate))/minutesInADay))
+  }
+
   // Based on the current user, organize the data by their teams
-  const organizeTeamData = (userEmail, Users) => {
+  const organizeTeamData = (userEmail, Users, startDates) => {
     let displayTeams = []; // Array of teams
     let playerTeams = []; // Names of the team the player is on
 
-    const currentUser = Users.find(player => player.fields.email === userEmail); // Find current player from database of players
-    
+    const currentUser = Users.find(player => player.fields.email === userEmail); // Find current player from database of players    
     playerTeams = currentUser.fields.teams.split(','); // Get current player teams and convert into an array
 
     // Create array of array of players by team name
     playerTeams.forEach(team => {
       let teamDetails = {};
       teamDetails.teamName = team;
-      teamDetails.currentWeek = 1;/**TODO: Functionality & data to dynamically get current week */
+      teamDetails.currentWeek = getCurrentWeek(startDates, team);/**dynamically get current week */
       let teamOfPlayers = [];
       Users.forEach(player => {
         const checkIfOnSameTeam = player.fields.teams.includes(team);
-        const minutesInADay = 86400000; // Number of milliseconds in a day
-        const daysSinceLastUpdate = Math.round(Math.abs((Date.parse(new Date()) - Date.parse(player.fields.lastUpdate))/minutesInADay))
+        const daysSinceLastUpdate = determineLastUpdate(player.fields.lastUpdate) 
         if(checkIfOnSameTeam){
           /*Strip all players of un-needed data*/
           const sanitizedPlayer = {};
           sanitizedPlayer.playerName = player.fields.playerName;
           sanitizedPlayer.weightLoss = player.fields.weightLoss;
           sanitizedPlayer.winner = player.fields.winner;
-          sanitizedPlayer.lastUpdate = daysSinceLastUpdate
+          sanitizedPlayer.lastUpdate = daysSinceLastUpdate;
           teamOfPlayers.push(sanitizedPlayer);
         }
       });
-      teamDetails.players = teamOfPlayers; // Add teams of players to array of teams
 
+      teamDetails.players = teamOfPlayers; // Add teams of players to array of teams
       displayTeams.push(teamDetails);// Add teams of players to array of teams
     })
     return displayTeams;
